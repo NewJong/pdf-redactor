e3ce15fda4fcb988e60f72095435575b41b51491
NewJong <rkhoroshev1508@gmail.com>
2025-06-16 01:01:54 +0300

    Test 2

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+APP = ['pdf_editor.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['PIL', 'fitz'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
